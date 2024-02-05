e2ea723cae63384f3ac94958d442fbf13769c7ac
leehendrick <imhendrick16@gmail.com>
2024-02-05 11:27:05 +0100

    Criando o controler e o model das receitas

---
+++ app/Http/Controllers/ReceitaController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ReceitaController extends Controller
+{
+    public function index()
+    {
+
+    }
+
+    public function create()
+    {
+
+    }
+
+    public function store()
+    {
+
+    }
+
+    public function show()
+    {
+
+    }
+
+    public function update()
+    {
+
+    }
+
+    public function destroy()
+    {
+
+    }
+}
