464ad1bf1dad593d47b220bbefe6d93848e0720f
leehendrick <imhendrick16@gmail.com>
2024-02-06 13:20:14 +0100

    Criando o controller da página home

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Inertia\Inertia;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        if(Auth::id())
+        {
+            $usertype = Auth()->user()->user_type;
+            if ($usertype == 'user'){
+                return Inertia::render('Dashboard');
+            }
+            else if($usertype == 'admin'){
+                return view('receita.comfirm');
+            }
+            else{
+                redirect()->back();
+            }
+        }
+    }
+}
