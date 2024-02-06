f68827e67d28a81b274fd8afa4513a77747b1624
leehendrick <imhendrick16@gmail.com>
2024-02-06 13:16:40 +0100

    Definindo níveis de acesso para usuario e admin

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ReceitaController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -29,12 +31,18 @@ Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/home', [HomeController::class, 'index'])->middleware(['auth',])->name('home');
+
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+
+    Route::resource('receita', ReceitaController::class);
 });
 
-Route::resource('receita', \App\Http\Controllers\ReceitaController::class);
+
 
 require __DIR__.'/auth.php';
