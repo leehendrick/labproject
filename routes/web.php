95762bc6fea51ef775f3f81ca76b597c2f81b23e
leehendrick <imhendrick16@gmail.com>
2024-02-07 10:42:37 +0100

    Definindo o middleware para o crud

--- routes/web.php
+++ routes/web.php
@@ -38,11 +38,11 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
-
-    Route::resource('receita', ReceitaController::class);
 });
 
+Route::middleware(['auth', 'admin'])->group(function (){
+    Route::resource('/receita', ReceitaController::class);
+});
 
 
 require __DIR__.'/auth.php';
