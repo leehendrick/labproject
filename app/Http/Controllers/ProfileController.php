24fa33f2321d8b0a8ba1e2edec81533dbedee9ae
leehendrick <imhendrick16@gmail.com>
2024-02-08 12:24:17 +0100

    Resolvendoa gambiarra da edição de perfil

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -40,13 +40,6 @@ class ProfileController extends Controller
 
     }
 
-    /*public function editAdmin(Request $request): Response
-    {
-        return Inertia::render('Profile/EditAdmin', [
-           'mustVerifyEmail' => $request->user() instanceof MustVerifyEmail,
-            'status' => session('status'),
-        ]);
-    }*/
 
     /**
      * Update the user's profile information.
