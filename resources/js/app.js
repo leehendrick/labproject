d16532464be1fef531523c3cbc140b5ddd041be5
leehendrick <imhendrick16@gmail.com>
2024-02-15 12:51:15 +0100

    Adicionando o vue router

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,14 +9,28 @@ import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
 import VueSweetalert2 from 'vue-sweetalert2';
 import 'sweetalert2/dist/sweetalert2.min.css';
+import {createRouter, createWebHistory} from "vue-router";
 
 
+//Chamando os componentes Vue
+import Dashboard from "@/Pages/Dashboard.vue";
+import Home from "@/Pages/Home.vue";
+import Index from "@/Pages/index.vue";
+
+const routes = [
+    { path: '/index', component: Index },
+    { path: '/home', component: Home },
+    { path: '/dashboard', component: Dashboard },
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
 
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 const el = document.getElementById('app');
-
-
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
