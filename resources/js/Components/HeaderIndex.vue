9958639e51f765a4ec82d4865c32ffa61654bffc
leehendrick <imhendrick16@gmail.com>
2024-02-15 12:53:35 +0100

    registro de formandos

---
+++ resources/js/Components/HeaderIndex.vue
@@ -0,0 +1,35 @@
+<script setup>
+const navigation = [
+  { name: 'Home', href: '/index' },
+  { name: 'Inscrições', href: '/registro' },
+  { name: 'Contatos', href: '/contatos' },
+]
+</script>
+
+<template>
+  <header class="bg-secondary top-0 fixed w-full ">
+    <nav class="mx-auto max-w-7xl px-6 lg:px-8" aria-label="Top">
+      <div class="flex w-full items-center justify-between border-b border-indigo-500 py-6 lg:border-none">
+        <div class="flex items-center">
+          <a href="#">
+            <span class="sr-only">Your Company</span>
+          </a>
+          <div class="ml-10 hidden space-x-8 lg:block">
+            <a v-for="link in navigation" :key="link.name" :href="link.href" class="text-base font-medium text-white hover:text-indigo-50">{{ link.name }}</a>
+          </div>
+        </div>
+        <div class="ml-10 space-x-4">
+          <a href="#" class="inline-block rounded-md border border-transparent bt-hover px-4 py-2 text-base font-medium text-white hover:bg-opacity-75">Sign in</a>
+          <a href="#" class="inline-block rounded-md border border-transparent bg-white px-4 py-2 text-base font-medium text-primary hover:bg-indigo-50">Sign up</a>
+        </div>
+      </div>
+      <div class="flex flex-wrap justify-center gap-x-6 py-4 lg:hidden">
+        <a v-for="link in navigation" :key="link.name" :href="link.href" class="text-base font-medium text-white hover:text-indigo-50">{{ link.name }}</a>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<style scoped>
+
+</style>
---
+++ resources/js/Pages/index.vue
@@ -0,0 +1,41 @@
+<script setup>
+
+import HeaderIndex from "@/Components/HeaderIndex.vue";
+</script>
+
+<template>
+<header-index/>
+  <main class="mt-40 container mx-auto p-4">
+
+    <div class="flex flex-col md:flex-row">
+      <!-- Container do lado esquerdo -->
+      <div class=" w-full md:w-3/5 bg-white p-4">
+        <h2 class="text-2xl font-bold mb-4">Cursos Disponíveis</h2>
+
+        <div class="max-w-lg mx-auto mt-8 bg-white shadow-md rounded p-6">
+          <h2 class="text-2xl font-bold mb-4">Curso de Tailwind CSS</h2>
+          <p class="text-gray-600 mb-4">Vagas Disponíveis: 10</p>
+          <button class="bg-suave bt-hover text-white font-bold py-2 px-4 rounded">
+            Inscrever-se
+          </button>
+        </div>
+      </div>
+
+      <!-- Linha vertical separadora -->
+      <hr class="border-layout"/>
+
+      <!-- Container do lado direito -->
+      <div class="w-full md:w-1/4 bg-white p-4">
+        <h2 class="text-2xl font-bold mb-4">Notas Informativas</h2>
+        <!-- Conteúdo do container direito aqui -->
+        <div class="overflow-auto break-words">
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
