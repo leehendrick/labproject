f918a88972319cd9b626fc20c88767ba3f64b0d6
leehendrick <imhendrick16@gmail.com>
2024-02-19 12:38:08 +0100

    separando a lógica do vue router

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,33 +1,12 @@
 import './bootstrap';
 import '../css/app.css';
-
-
-
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
 import VueSweetalert2 from 'vue-sweetalert2';
 import 'sweetalert2/dist/sweetalert2.min.css';
-import {createRouter, createWebHistory} from "vue-router";
-
-
-//Chamando os componentes Vue
-import Dashboard from "@/Pages/Dashboard.vue";
-import Home from "@/Pages/Home.vue";
-import Index from "@/Pages/index.vue";
-
-const routes = [
-    { path: '/index', component: Index },
-    { path: '/home', component: Home },
-    { path: '/dashboard', component: Dashboard },
-];
-
-const router = createRouter({
-    history: createWebHistory(),
-    routes,
-});
-
+import router from './router.js'
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 const el = document.getElementById('app');
@@ -40,10 +19,11 @@ createInertiaApp({
             .use(plugin)
             .use(ZiggyVue)
             .use(VueSweetalert2)
+            .use(router)
             .mount(el);
 
     },
     progress: {
-        color: '#4B5563',
+        color: '#FFC423',
     },
 })
