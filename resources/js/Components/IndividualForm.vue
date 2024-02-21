267b827d75058740d870c023df53a9071f5b1f60
leehendrick <imhendrick16@gmail.com>
2024-02-21 13:40:05 +0100

    Separando formularios e chamando consoante ao select

---
+++ resources/js/Components/IndividualForm.vue
@@ -0,0 +1,74 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="max-w-md mx-auto mt-8 p-6 bg-white rounded-md shadow-md w-full md:w-3/5">
+    <h2 class="text-2xl font-semibold mb-4">Formulário de Inscrição</h2>
+
+    <!-- Nome Completo -->
+    <div class="mb-4">
+      <label for="nome" class="block text-sm font-medium text-gray-600">Nome Completo</label>
+      <input type="text" id="nome" name="nome" class="mt-1 p-2 w-full border rounded-md" placeholder="Digite seu nome completo">
+    </div>
+
+    <!-- Sexo -->
+    <div class="mb-4">
+      <label class="block text-sm font-medium text-gray-600">Sexo</label>
+      <div class="mt-1">
+        <label class="inline-flex items-center">
+          <input type="radio" class="form-radio" name="sexo" value="masculino">
+          <span class="ml-2">Masculino</span>
+        </label>
+        <label class="inline-flex items-center ml-6">
+          <input type="radio" class="form-radio" name="sexo" value="feminino">
+          <span class="ml-2">Feminino</span>
+        </label>
+      </div>
+    </div>
+
+    <!-- Número do BI -->
+    <div class="mb-4">
+      <label for="bi" class="block text-sm font-medium text-gray-600">Número do BI</label>
+      <input type="text" id="bi" name="bi" class="mt-1 p-2 w-full border rounded-md" placeholder="Digite o número do BI">
+    </div>
+
+    <!-- Número de Telefone -->
+    <div class="mb-4">
+      <label for="telefone" class="block text-sm font-medium text-gray-600">Número de Telefone</label>
+      <input type="tel" id="telefone" name="telefone" class="mt-1 p-2 w-full border rounded-md" placeholder="Digite o número de telefone">
+    </div>
+
+    <!-- Instituição de Ensino -->
+    <div class="mb-4">
+      <label for="instituicao" class="block text-sm font-medium text-gray-600">Instituição de Ensino</label>
+      <input type="text" id="instituicao" name="instituicao" class="mt-1 p-2 w-full border rounded-md" placeholder="Digite o nome da instituição de ensino">
+    </div>
+
+    <!-- Grau Acadêmico -->
+    <div class="mb-4">
+      <label class="block text-sm font-medium text-gray-600">Grau Acadêmico</label>
+      <select class="mt-1 p-2 w-full border rounded-md">
+        <option value="bacharelado">Escolha uma opção</option>
+        <option value="bacharelado">Ensino de Base</option>
+        <option value="licenciatura">Ensino Médio</option>
+        <option value="mestrado">1º Ano - Licenciatura</option>
+        <option value="doutorado">2º Ano - Licenciatura</option>
+        <option value="doutorado">3º Ano - Licenciatura</option>
+        <option value="doutorado">4º Ano - Licenciatura</option>
+        <option value="doutorado">5º Ano - Licenciatura</option>
+        <option value="doutorado">Licenciado</option>
+        <option value="doutorado">Mestre ou Superior</option>
+      </select>
+    </div>
+
+    <!-- Botão de Envio -->
+    <button type="submit" class="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600">Enviar</button>
+  </div>
+
+
+</template>
+
+<style scoped>
+
+</style>
--- resources/js/Pages/Index/registro.vue
+++ resources/js/Pages/Index/registro.vue
@@ -1,10 +1,52 @@
 <script setup>
 import HeaderIndex from "@/Components/HeaderIndex.vue";
+import { ref, onMounted } from "vue";
+import IndividualForm from "@/Components/IndividualForm.vue";
+import axios from 'axios';
+import { useRoute } from 'vue-router';
+
+const selectedFormType = ref('default');
+
+const handleSelectChange = () => {
+  //
+};
+
+const cursos = ref({});
+const route = useRoute();
+
+onMounted(async () => {
+  try {
+    const response = await axios.get(`/api/cursos`);
+    cursos.value = response.data;
+  } catch (error) {
+    console.error(error);
+  }
+});
 </script>
 
 <template>
   <header-index/>
-  <h1 class="mt-40">Registro</h1>
+  <main class="mt-16 container mx-auto p-4">
+
+    <div class="flex flex-col md:flex-row">
+      <!-- Container do lado esquerdo -->
+      <div class="w-full md:w-3/5 bg-white p-4">
+        <h2 class="text-2xl font-bold mb-4 mt-6">Inscrição CCD</h2>
+        <div class="mt-2">
+          <select v-model="selectedFormType" @change="handleSelectChange" id="typeForm" name="typeForm" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6">
+            <option selected disabled>Selecione o tipo de inscrição</option>
+            <option>Individual</option>
+            <option>Empresarial</option>
+          </select>
+        </div>
+        <hr class="border-layout mt-10"/>
+        <div id="fillForm" class="mt-6">
+          <individual-form v-if="selectedFormType === 'Individual'"></individual-form>
+        </div>
+      </div>
+
+      </div>
+  </main>
 </template>
 
 <style scoped>
