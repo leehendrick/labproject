276db90d40603a9896dccdf83d3a6e7aaa9d1559
leehendrick <imhendrick16@gmail.com>
2024-02-23 12:42:48 +0100

    Refactorando

--- resources/js/Pages/Index/registro.vue
+++ resources/js/Pages/Index/registro.vue
@@ -22,17 +22,21 @@ const handleSelectChange = () => {
         <h2 class="text-2xl font-bold mb-4 mt-6">Inscrição CCD</h2>
         <div class="mt-2">
           <select v-model="selectedFormType" @change="handleSelectChange" id="typeForm" name="typeForm" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6">
-            <option selected disabled>Selecione o tipo de inscrição</option>
+            <option>Selecione o tipo de inscrição</option>
             <option>Individual</option>
             <option>Empresarial</option>
           </select>
         </div>
-        <hr class="border-layout mt-10"/>
-        <div id="fillForm" class="mt-6">
-          <individual-form v-if="selectedFormType === 'Individual'"></individual-form>
+        <div class="bg-transparent border-layout rounded-md shadow-md mt-6 h-screen flex items-center justify-center">
+        <div class="m-6 w-full">
+          <!-- Conteúdo do container -->
+          <div id="fillForm">
+            <individual-form v-if="selectedFormType === 'Individual'"></individual-form>
+          </div>
+        </div>
         </div>
       </div>
-
+      <p class="lg:mt-36 ml-4">Something here.</p>
       </div>
   </main>
 </template>
