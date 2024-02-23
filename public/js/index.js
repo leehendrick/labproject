3903be767cdbd9d0e4cefbb85925653530170a51
leehendrick <imhendrick16@gmail.com>
2024-02-23 12:42:20 +0100

    Função para inserir inscrições na bd

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,9 @@
 
 import { ref, onMounted } from 'vue';
 import axios from 'axios';
+import Swal from "sweetalert2";
+
+
 
 
 //Função para retornar os cursos no componente index
@@ -20,3 +23,57 @@ export default function useCursos() {
         cursos,
     };
 }
+
+export const inscricao = {
+    nome: '',
+    email: '',
+    sexo: '',
+    bi: '',
+    instituicao: '',
+    grau_academico: '',
+    tipo: 'individual',
+};
+
+function LimparCampos(){
+    inscricao.nome = '';
+    inscricao.email = '';
+    inscricao.sexo = '';
+    inscricao.bi = '';
+    inscricao.instituicao = '';
+    inscricao.grau_academico = '';
+}
+export async  function fetchInscricao(id) {
+    try {
+        const response = await axios.get(`/api/inscricoes/${id}`);
+        Object.assign(inscricao, response.data);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export async function submitInscricao(){
+    try{
+        console.log(inscricao)
+        await axios.post('/api/inscricoes', inscricao);
+        Swal.fire({
+            icon: 'success',
+            title: 'Inscrição realizada com sucesso!',
+            showConfirmButton: false,
+            timer: 1500,
+        })
+        LimparCampos();
+
+    } catch (error){
+        console.log('[ERRO AO INSERIR NA BD]:', error);
+        Swal.fire({
+            icon: 'error',
+            title: 'Erro ao realizar inscrição',
+            text: `${error}`,
+            showConfirmButton: true,
+        })
+    }
+
+}
+
+
+
