9ba9a73557a874e21c8c1e4b8137fdb621602635
leehendrick <imhendrick16@gmail.com>
2024-03-04 13:48:06 +0100

    removendo input sobrenome

--- resources/js/Pages/Index/contatos.vue
+++ resources/js/Pages/Index/contatos.vue
@@ -51,15 +51,9 @@
           <div class="mx-auto max-w-xl lg:mr-0 lg:max-w-lg">
             <div class="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
               <div>
-                <label for="first-name" class="block text-sm font-semibold leading-6 text-gray-900">First name</label>
+                <label for="first-name" class="block text-sm font-semibold leading-6 text-gray-900">Seu name</label>
                 <div class="mt-2.5">
                   <input type="text" name="first-name" id="first-name" autocomplete="given-name" class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 my-ring2 sm:text-sm sm:leading-6" />
-                </div>
-              </div>
-              <div>
-                <label for="last-name" class="block text-sm font-semibold leading-6 text-gray-900">Last name</label>
-                <div class="mt-2.5">
-                  <input type="text" name="last-name" id="last-name" autocomplete="family-name" class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 my-ring2 sm:text-sm sm:leading-6" />
                 </div>
               </div>
               <div class="sm:col-span-2">
