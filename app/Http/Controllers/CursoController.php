d6c0e839c2815d2a797ebb26576c2792eeda13e4
leehendrick <imhendrick16@gmail.com>
2024-03-11 13:31:09 +0100

    update file

--- app/Http/Controllers/CursoController.php
+++ app/Http/Controllers/CursoController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Curso;
 use Inertia\Inertia;
-
+use Illuminate\Http\Request;
 class CursoController extends Controller
 {
     public function index()
@@ -12,9 +12,25 @@ class CursoController extends Controller
         return Curso::all();
     }
 
-    public function store ()
+    public function store (Request $request)
     {
+      /*  $request->validate([
+            'nome' => 'required',
+            'descricao' => 'required',
+            'data_inicio' => 'required',
+            'data_termino' => 'required',
+            'vagas' => 'required',
+            'local' => 'required',
+            'preco' => 'required',
+            'modulos' => 'required',
+            'nota' => 'required',
+            'requisitos' => 'required',
+            'duracao' => 'required',
+            'horario' => 'required',
+            'criado_por' => 'required',
+        ]);*/
 
+        return Curso::create($request->all());
     }
 
     public function show ($id)
